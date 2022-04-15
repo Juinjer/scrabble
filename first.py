afaeabb38514e48ae1d8b376f21b7d734cd214e2
Stef Vanaken <stef.vanaken1@student.kuleuven.be>
2022-04-15 23:09:27 +0200

    play best value instead of best word

--- first.py
+++ first.py
@@ -1,29 +1,29 @@
-from operator import index
 import util
 from copy import deepcopy
+
 
 class Scrabble:
     def __init__(self, field: int, list: str = 'TWL06') -> None:
         self.wordlist = util.getWordList(list)
         self.field = self.initfield(field, '  ')
         self.valuemap = self.initfield(field, 0)
+        self.updateMultipliers(self.valuemap)
         self.reprField(self.field)
         self.play()
-        self.playplaces = set((field//2+1,field//2+1))
 
-    def initfield(self, dim: int, fill: str|int) -> list[list[str | int]]:
+    def initfield(self, dim: int, fill: str | int) -> list[list[str | int]]:
         arr = [[fill for i in range(dim)] for j in range(dim)]
         return arr
-    
+
     def reprField(self, field: list[list[str]]):
-        print('   |',end='')
+        print('   |', end='')
         for i in range(len(field)):
-            print('{:2}'.format(i),end='|')
+            print('{:2}'.format(i), end='|')
         print()
         for i in range(len(field)):
             print('{:2} |'.format(i), end='')
             for j in range(len(field[0])):
-                print('{:2}'.format(field[i][j]),end='|')
+                print('{:2}'.format(field[i][j]), end='|')
             print()
 
     def play(self):
@@ -36,10 +36,14 @@ class Scrabble:
                 if boardletters:
                     a = dict()
                     for letter in boardletters:
-                        a = a | self.checkBest(avail+letter)
+                        a = a | self.checkBest(avail + letter)
                     res = util.sortDict(a)
-                    self.displayOptions(res, avail)
-                    self.tempFill(res,avail)
+                    # self.displayOptions(res, avail)
+                    tempboard = self.tempFill(res, avail)
+                    fill = input('Want to fill? [y/n]: ')
+                    if fill == 'y':
+                        self.field = tempboard[0]
+                        self.valuemap = tempboard[1]
                 else:
                     res = self.checkBest(avail)
                     sor = util.sortDict(res)
@@ -49,11 +53,18 @@ class Scrabble:
                 pos = input("Position (fe. 00 00|25 25): ")
                 dir = input("Direction (d|r): ")
                 positions = util.parsePos(pos, dir, len(word))
-                self.fillWord(positions,word)
+                self.fillWord(positions, word)
                 self.reprField(self.field)
+            elif 'q' == choice:
+                finished = False
             else:
                 print("enter a valid option 0|1")
-    
+
+    # def updateMultipliers(self) -> None:
+    #     width = len(self.valuemap)  # TODO make dynamic
+    #     w2 = [(1, 5), (1, 9), (1, 17), (1, 21), (3, 1), (3, 14), (3, 25), (5, 5), (5, 9), (5, 17), (5, 21), (7, 4), (7, 10), (7, 16), (7, 22)]
+    #     pass
+
     def checkValidBoard(self, board: list[list[str]]) -> bool:
         for line in board:
             valid = self.checkLine(line)
@@ -74,7 +85,6 @@ class Scrabble:
                 return False
         return True
 
-
     def getBoardLetter(self) -> set[str]:
         res = set()
         for line in self.field:
@@ -82,98 +92,95 @@ class Scrabble:
                 res.add(elem)
         res.remove('  ')
         return res
-    
+
     def checkBest(self, available: str):
         temp = dict()
         if '?' in available:
-            avalist = util.getWildcard(available)
-            variantions = set()
-            for elem in avalist:
-                x = util.permutations(elem, self.wordlist)
-                variantions = variantions.union(x)
+            variantions = util.permutations(available, self.wordlist, True)
         else:
-            variantions = util.permutations(available, self.wordlist)
+            variantions = util.permutations(available, self.wordlist, False)
         for var in variantions:
-            # print(var)
             if var in self.wordlist:
-                # print(var)
-                wildcard = util.disjoin(var,available)
+                wildcard = util.disjoin(var, available)
                 temp[var] = util.getValue(var, wildcard)
         # print(util.sortDict(temp))
         return temp
-    
+
     def fillWord(self, positions: list[tuple[int, int]], word: str) -> None:
         tempboard = deepcopy(self.field)
         tempvalues = deepcopy(self.valuemap)
-        i = 0
-        for elem in positions:
+        for char, elem in zip(word, positions):
             if tempboard[elem[0]][elem[1]] == '  ':
-                tempboard[elem[0]][elem[1]] = word[i]
-                tempvalues[elem[0]][elem[1]] = util.values.get(word[i])
-            i+=1
+                tempboard[elem[0]][elem[1]] = char
+                tempvalues[elem[0]][elem[1]] = util.updateValues((elem[0], elem[1]), util.values.get(char), self.valuemap)
         if self.checkValidBoard(tempboard):
             self.field = tempboard
+            self.valuemap = tempvalues
         else:
             print("not a valid word/position")
 
     def displayOptions(self, wordoptions: dict[str, int], available: str):
-        i = 0
-        for w in wordoptions:
+        for i, w in enumerate(wordoptions):
             boardletter = util.disjoin(w, available)
             if available:
                 print(f'Play {w}, value: {wordoptions[w]} through letter {boardletter}')
             else:
                 print(f'Play {w}, value: {wordoptions[w]}')
-            if i == 19:
+            if i == 20:
                 break
-            i += 1
-    
-    def tempFill(self, wordoptions: dict[str, int], available: str) -> list[list[dict[str, int]]]:
-        for w in wordoptions:
-            boardletter = util.disjoin(w, available)
-            index = w.find(boardletter)
-            if boardletter:
-                for (posy,posx) in self.getPositions(boardletter):
+
+    def tempFill(self, wordoptions: dict[str, int], available: str):
+        bestvalue = 0
+        best = (self.field, self.valuemap)
+        for word in wordoptions:
+            boardletter = util.disjoin(word, available)
+            index = word.find(boardletter)
+            if boardletter:  # TODO: implement without boardletter fe at edge of word
+                for (posy, posx) in self.getPositions(boardletter):
                     tempboard = deepcopy(self.field)
                     tempvalues = deepcopy(self.valuemap)
                     beginh = posx - index
-                    endh = posx - index + len(w)
-                    j = 0
+                    endh = posx - index + len(word)
                     valid = True
-                    for i in range(beginh,endh):
+                    for char, i in zip(word, range(beginh, endh)):
                         if tempboard[posy][i] == '  ' or tempboard[posy][i] == boardletter:
-                            tempboard[posy][i] = w[j]
-                            tempvalues[posy][i] = util.values.get(w[j])
+                            tempboard[posy][i] = char
+                            tempvalues[posy][i] = util.updateValues((posy, i), util.values.get(char), self.valuemap)
                         else:
                             valid = False
-                        j +=1
-                    if self.checkValidBoard(tempboard) and valid:
-                        self.reprField(tempboard)
-                        return tempboard
+                    newval = util.getMapValue(tempboard, self.field)
+                    if self.checkValidBoard(tempboard) and valid and newval > bestvalue:
+                        bestvalue = newval
+                        best = (tempboard, tempvalues)
+                        # self.reprField(tempboard)
+                        # return tempboard
                     tempboard = deepcopy(self.field)
                     beginv = posy - index
-                    endv = posy - index + len(w)
-                    j = 0
+                    endv = posy - index + len(word)
                     valid = True
-                    for i in range(beginv,endv):
+                    for char, i in zip(word, range(beginv, endv)):
                         if tempboard[i][posx] == '  ' or tempboard[i][posx] == boardletter:
-                            tempboard[i][posx] = w[j]
-                            tempvalues[i][posx] = util.values.get(w[j])
+                            tempboard[i][posx] = char
+                            tempvalues[i][posx] = util.updateValues((i, posx), util.values.get(char), self.valuemap)
                         else:
                             valid = False
-                        j +=1
-                    if self.checkValidBoard(tempboard) and valid:
-                        self.reprField(tempboard)
-                        return tempboard
-    
-    def getPositions(self, letter: str)-> str:
+                    newval = util.getMapValue(tempboard, self.field)
+                    if self.checkValidBoard(tempboard) and valid and newval > bestvalue:
+                        bestvalue = newval
+                        best = (tempboard, tempvalues)
+                        # self.reprField(tempboard)
+                        # return tempboard
+        self.reprField(best[0])
+        return best
+
+    def getPositions(self, letter: str) -> str:
         res = []
         for i in range(len(self.field)):
             for j in range(len(self.field[0])):
                 if letter == self.field[i][j]:
-                    res.append((i,j))
+                    res.append((i, j))
         return res
-        
-        
+
+
 if __name__ == "__main__":
-    Scrabble(30)
+    Scrabble(27)
--- util.py
+++ util.py
@@ -1,5 +1,4 @@
-import itertools
-import time
+from typing import Tuple
 values = {
     'a': 1,
     'b': 3,
@@ -29,11 +28,13 @@ values = {
     'z': 10,
     '  ': 0,
     '?': 0,
-    '' : 0
+    '': 0
 }
 alfabet = "abcdefghijklmnopqrstuvwxyz"
+
+
 def process(path: str):
-    with open(f'{path}.txt','r') as s:
+    with open(f'{path}.txt', 'r') as s:
         wordlist = s.readlines()
     res = []
     for word in wordlist:
@@ -41,11 +42,12 @@ def process(path: str):
         temp = 0
         for letter in word:
             # print(letter)
-            temp+= values.get(letter.lower())
+            temp += values.get(letter.lower())
         res.append(word + ' ' + str(temp) + '\n')
 
-    with open(f'{path}res.txt','w') as f:
+    with open(f'{path}res.txt', 'w') as f:
         f.writelines(res)
+
 
 def getValue(s: str, wildcard: str):
     res = 0
@@ -55,46 +57,66 @@ def getValue(s: str, wildcard: str):
         res -= values.get(letter)
     return res
 
+
 def parse(path: str):
-    with open(f'{path}.txt','r') as r:
+    with open(f'{path}.txt', 'r') as r:
         temp = r.readlines()
     wr = [f'{line.split()[0]}\n' for line in temp]
-    with open(f'{path}Parsed.txt','w') as w:
+    with open(f'{path}Parsed.txt', 'w') as w:
         w.writelines(wr)
 
+
 def getWordList(path: str) -> set[str]:
-    with open(f'{path}.txt','r') as f:
+    with open(f'{path}.txt', 'r') as f:
         wordlist = f.readlines()
     return set([word.strip('\n').lower() for word in wordlist])
 
-def getWildcard(s: str) -> set[tuple[str,str]]:
-    res = set()
-    count = s.count('?')
-    temp = s.strip('?')
-    test = [''.join(p) for p in itertools.product(alfabet,repeat = count)]
-    a = set(tuple(sorted(x)) for x in test)
-    for elem in a:
-        b = temp + ''.join(elem) 
-        res.add(b)
-    return res
+# def wildcarPerm(s: str, wordlist: set[str]) -> None:
+#     res = set()
+#     count = s.count('?')
+#     temp = s.strip('?')
+#     for word in wordlist:
+#         if checkWord(word, temp, count):
+#             res.add(word)
+#     return res
 
-def permutations(s: str, wordlist:set[str]) -> set:
+# def getWildcard(s: str) -> set[tuple[str,str]]:
+#     res = set()
+#     count = s.count('?')
+#     temp = s.strip('?')
+#     test = [''.join(p) for p in itertools.product(alfabet,repeat = count)]
+#     a = set(tuple(sorted(x)) for x in test)
+#     for elem in a:
+#         b = temp + ''.join(elem)
+#         res.add(b)
+#     return res
+
+
+def permutations(s: str, wordlist: set[str], wildcard: bool = False) -> set:
     a = set()
-    # first = time.time()
-    for word in wordlist:
-        if checkWord(word,s):
-            a.add(word)
-    # second = time.time()
-    # print("2 " + str(second-first))
+    if wildcard:
+        count = s.count('?')
+        temp = s.strip('?')
+        for word in wordlist:
+            if checkWord(word, temp, count):
+                a.add(word)
+    else:
+        for word in wordlist:
+            if checkWord(word, s):
+                a.add(word)
     return a
 
-def checkWord(word: str, available: str)-> bool:
+
+def checkWord(word: str, available: str, wildcards: int = 0) -> bool:
     # if word == 'bechalking':
     #     print(word)
     for x in word:
         if x not in available:
-            return False
-        available = available.replace(x,'',1)
+            if wildcards == 0:
+                return False
+            else:
+                wildcards -= 1
+        available = available.replace(x, '', 1)
     return True
 
 # def permutations(s: str, wordlist:set[str]) -> set:
@@ -109,32 +131,64 @@ def checkWord(word: str, available: str)-> bool:
 #     print("1 " + str(second-first))
 #     return wordlist.intersection(a)
 
+
 def disjoin(full: str, part: str):
     for letter in part:
-        full = full.replace(letter,'',1)
+        full = full.replace(letter, '', 1)
     return full
 
-def parsePos(s: str, dir: str, length: int) -> list[tuple[int,int]]:
-    res: list[tuple[int,int]] = []
+
+def parsePos(s: str, dir: str, length: int) -> list[tuple[int, int]]:
+    res: list[tuple[int, int]] = []
     posx, posy = s.split()
-    first = (int(posx),int(posy))
+    first = (int(posx), int(posy))
     # second = (int(pos2[0]), int(pos2[1]))
     # diff = (abs(second[0]-first[0]),abs(second[1]-first[1]))
     if dir == 'r':
         for i in range(first[1], first[1] + length):
-            res.append((first[0],i))
+            res.append((first[0], i))
     else:
         for i in range(first[0], first[0] + length):
-            res.append((i,first[1]))
+            res.append((i, first[1]))
     return res
+
 
 def sortDict(dic: dict) -> dict:
     return dict(sorted(dic.items(), key=lambda item: item[1], reverse=True))
 
-def sortList(lis: list[tuple[str,int]]) -> list[tuple[str,int]]:
-    return sorted(lis ,key=lambda tup: tup[1],reverse=True)
+
+def sortList(lis: list[tuple[str, int]]) -> list[tuple[str, int]]:
+    return sorted(lis, key=lambda tup: tup[1], reverse=True)
+
+
+def getMapValue(maps: list[list[str]], oldmap: list[list[str]]) -> int:
+    res = 0
+    # for line in maps:
+    #     res += sum(line)
+    for word in getAllWords(maps):
+        res += getValue(word, '')
+
+    for word in getAllWords(oldmap):
+        res -= getValue(word, '')
+    return res
+
+
+def getAllWords(stringmap: list[list[str]]) -> list[str]:
+    words = []
+    for line in stringmap:
+        word = ''.join(line)
+        words.extend([x for x in word.split('  ') if len(x) > 1])
+    for i in range(len(stringmap[0])):
+        word = ''.join([stringmap[j][i] for j in range(len(stringmap))])
+        words.extend([x for x in word.split('  ') if len(x) > 1])
+    return words
+
+
+def updateValues(position: Tuple[int, int], value: int, map: list[list[str | int]]) -> None:
+    map[position[0]][position[1]] = value
+
 
 if __name__ == '__main__':
-    print(permutations("abcdefghi",getWordList('NWL2020Parsed')))
+    print(permutations("abcdefghi", getWordList('NWL2020Parsed')))
     # print(permutations2("abcdefghi",getWordList('NWL2020Parsed')))
     pass
